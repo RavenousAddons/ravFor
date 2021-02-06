f007faafab0956da51a06b045921a79f20a8ba78
waldenp0nd <20667094+waldenp0nd@users.noreply.github.com>
2021-02-06 06:07:06 +0000

    v0.0.3-alpha

--- data.lua
+++ data.lua
@@ -7,147 +7,274 @@ ravShadowlands.data = {
         {
             -- Maldraxxus
             id = 1536,
+            covenant = 4,
             color = "40bf40",
-            bosses = {
+            icon = "3257749",
+            rares = {
                 {
                     id = 162819,
                     name = "Warbringer Mal'korak",
                     waypoint = {33.7, 80.1},
                     quest = 58889,
-                    item = 182085,
-                    mount = 1372,
+                    items = {
+                        {
+                            id = 182085,
+                            mount = 1372,
+                        },
+                    },
                 },
                 {
                     id = 162690,
                     name = "Nerissa Heartless",
                     waypoint = {66.1, 35.3},
                     quest = 58851,
-                    item = 182084,
-                    mount = 1373,
+                    items = {
+                        {
+                            id = 182084,
+                            mount = 1373,
+                        },
+                    },
                 },
                 {
                     id = 162586,
                     name = "Tahonta",
                     waypoint = {44.2, 51.1},
                     quest = 58783,
-                    item = 182075,
-                    mount = 1366,
+                    items = {
+                        {
+                            id = 182075,
+                            mount = 1366,
+                        },
+                    },
                 },
                 {
                     id = 162741,
                     name = "Gieger",
                     waypoint = {31.5, 35.3},
                     quest = 58837,
-                    covenantOnly = true,
-                    item = 182080,
-                    mount = 1411,
+                    items = {
+                        {
+                            id = 182080,
+                            mount = 1411,
+                            covenantOnly = true,
+                        },
+                    },
                 },
                 {
                     id = 174108,
                     name = "Necromatic Anomaly",
                     waypoint = {72.4, 28.6},
                     quest = 58889,
-                    item = 181810,
+                    items = {
+                        {
+                            id = 181810,
+                        },
+                    },
                 },
                 {
                     id = 157309,
                     name = "Violet Mistake",
                     waypoint = {31.5, 35.3},
                     quest = 61720,
-                    item = 182079,
-                    mount = 1410,
+                    items = {
+                        {
+                            id = 182079,
+                            mount = 1410,
+                        },
+                    },
                 },
             }
         },
         {
             -- Revendreth
             id = 1525,
+            covenant = 2,
             color = "ff4040",
-            bosses = {
+            icon = "3257751",
+            rares = {
                 {
                     id = 166521,
                     name = "Famu the Infinite",
                     waypoint = {62.4, 47.1},
                     quest = 59869,
-                    item = 180582,
-                    mount = 1379,
+                    items = {
+                        {
+                            id = 180582,
+                            mount = 1379,
+                        },
+                    },
                 },
                 {
                     id = 165290,
                     name = "Harika the Horrid",
                     waypoint = {45.3, 79.3},
                     quest = 59612,
-                    item = 180461,
-                    mount = 1310,
+                    covenantRequired = true,
+                    items = {
+                        {
+                            id = 180461,
+                            mount = 1310,
+                        },
+                    },
                 },
                 {
                     id = 166679,
                     name = "Hopecrusher",
                     waypoint = {52.0, 51.8},
                     quest = 59900,
-                    item = 180581,
-                    mount = 1298,
+                    items = {
+                        {
+                            id = 180581,
+                            mount = 1298,
+                        },
+                    },
+                },
+            }
+        },
+        {
+            -- The Maw
+            id = 1543,
+            color = "e5cc80",
+            icon = "3743739",
+            rares = {
+                {
+                    id = 174827,
+                    name = "Gorged Shadehound",
+                    waypoint = {62.4, 47.1},
+                    -- quest = 61124,
+                    items = {
+                        {
+                            id = 184167,
+                            mount = 1304,
+                        },
+                    },
                 },
             }
         },
         {
             -- Bastion
             id = 1533,
+            covenant = 1,
             color = "68ccef",
-            bosses = {
+            icon = "3257748",
+            rares = {
                 {
                     id = 170548,
                     name = "Sundancer",
                     waypoint = {60.0, 93.6},
-                    item = 180773,
-                    mount = 1307,
+                    items = {
+                        {
+                            id = 180773,
+                            mount = 1307,
+                        },
+                    },
+                },
+                {
+                    id = 167078,
+                    name = "Wingflayer the Cruel",
+                    waypoint = {60.0, 93.6},
+                    quest = 60314,
+                    items = {
+                        {
+                            id = 182749,
+                            covenantOnly = true,
+                        },
+                    },
+                },
+                {
+                    id = 170623,
+                    name = "Dark Watcher",
+                    waypoint = {60.0, 93.6},
+                    quest = 60883,
+                    items = {
+                        {
+                            id = 184297,
+                        },
+                    },
+                },
+                {
+                    id = 156339,
+                    name = "Orstus and Sotiros",
+                    waypoint = {60.0, 93.6},
+                    quest = 61634,
+                    items = {
+                        {
+                            id = 184365,
+                        },
+                    },
                 },
                 {
                     id = 170832,
                     name = "Ascended Council",
                     waypoint = {60.0, 93.6},
                     quest = 60977,
-                    item = 183741,
-                    mount = 1426,
+                    items = {
+                        {
+                            id = 183741,
+                            mount = 1426,
+                        },
+                    },
                 },
             }
         },
         {
             -- Ardenweald
             id = 1565,
+            covenant = 3,
             color = "8b55d7",
-            bosses = {
+            icon = "3257750",
+            rares = {
                 {
                     id = 164112,
                     name = "Humon'gozz",
                     waypoint = {32.5, 30.5},
                     quest = 59157,
-                    item = 182650,
-                    mount = 1415,
+                    items = {
+                        {
+                            id = 182650,
+                            mount = 1415,
+                            guaranteed = true,
+                        },
+                    },
                 },
                 {
                     id = 168647,
                     name = "Valfir the Unrelenting",
                     waypoint = {30.3, 56.0},
                     quest = 61632,
-                    item = 180730,
-                    mount = 1393,
+                    covenantRequired = true,
+                    items = {
+                        {
+                            id = 180730,
+                            mount = 1393,
+                        },
+                    },
                 },
                 {
                     id = 164107,
                     name = "Gormtamer Tizo",
                     waypoint = {30.3, 56.0},
                     quest = 59145,
-                    item = 180725,
-                    mount = 1362,
+                    items = {
+                        {
+                            id = 180725,
+                            mount = 1362,
+                            guaranteed = true,
+                        },
+                    },
                 },
                 {
                     id = 168135,
                     name = "Swift Gloomhoof",
                     waypoint = {30.3, 56.0},
                     quest = 60306,
-                    item = 180728,
-                    mount = 1306,
+                    items = {
+                        {
+                            id = 180728,
+                            mount = 1306,
+                            guaranteed = true,
+                        },
+                    },
                 },
             }
         },
--- window.lua
+++ window.lua
@@ -3,8 +3,8 @@ local L = ravShadowlands.L
 
 local zones = ravShadowlands.data.zones
 
-local width = 500
-local height = 400
+local width = 350
+local height = 450
 
 local small = 6
 local medium = 12
@@ -18,21 +18,25 @@ Window:SetHeight(height)
 Window:SetPoint("CENTER", 0, 0)
 Window:EnableMouse(true)
 Window:SetMovable(true)
+Window:SetClampedToScreen(true)
 Window:RegisterForDrag("LeftButton")
 Window:SetScript("OnDragStart", function(self) self:StartMoving() end)
 Window:SetScript("OnDragStop", function(self) self:StopMovingOrSizing() end)
-Window:SetClampedToScreen(true)
 
--- This makes it close with ESC
-tinsert(UISpecialFrames, Window:GetName())
+local WindowBackground = Window:CreateTexture(nil, "BACKGROUND")
+WindowBackground:SetColorTexture(0, 0, 0, 0.85)
+WindowBackground:SetAllPoints(Window)
+Window.texture = WindowBackground
 
-local Texture = Window:CreateTexture(nil, "BACKGROUND")
-Texture:SetColorTexture(0, 0, 0, 0.8)
-Texture:SetAllPoints(Window)
-Window.texture = Texture
+local Close = CreateFrame("Button", nil, Window, "UIPanelCloseButton")
+Close:SetPoint("TOPRIGHT", Window, "TOPRIGHT")
+Close:RegisterForClicks("AnyUp")
+Close:SetScript("OnClick", function(self)
+    Window:Hide()
+end)
 
 local Parent = CreateFrame("ScrollFrame", nil, Window, "UIPanelScrollFrameTemplate")
-Parent:SetWidth(width - (medium * 2) - 22) -- 22 seems to be width of the scrollbar
+Parent:SetWidth(width - (medium * 2) - 18) -- 22 seems to be width of the scrollbar
 Parent:SetHeight(height - (medium * 2) - (large * 2))
 Parent:SetPoint("BOTTOMLEFT", Window, "BOTTOMLEFT", medium, medium)
 
@@ -41,6 +45,7 @@ Content:SetWidth(1)
 Content:SetHeight(1)
 Parent:SetScrollChild(Content)
 
+Window:Hide()
 Window:SetScript("OnShow", function()
     ravShadowlands:CreateLabel({
         name = "Heading",
@@ -50,39 +55,54 @@ Window:SetScript("OnShow", function()
         relativePoint = "TOPLEFT",
         fontObject = "GameFontNormalLarge",
         offsetX = medium,
+        offsetY = -12,
     })
     -- For each Zone
     for i, zone in ipairs(zones) do
         local mapName = C_Map.GetMapInfo(zone.id).name
-        -- Zone Heading
+        -- Zone
         ravShadowlands:CreateLabel({
             name = zone.id,
             parent = Content,
-            label = "|cff" .. zone.color .. mapName .. ":|r",
+            label = "|T" .. zone.icon .. ":16|t |cff" .. zone.color .. mapName .. "|r",
             relativeTo = (i == 1 and Content or nil),
             relativePoint = (i == 1 and "TOPLEFT" or nil),
-            offsetY = (i == 1 and 0 or nil),
+            offsetY = (i == 1 and 0 or -large*2),
             fontObject = "GameFontNormalLarge",
         })
-        -- For each Boss in the Zone
-        for _, bossData in ipairs(zone.bosses) do
-            local itemName, itemLink = GetItemInfo(bossData.item)
-            local owned = false
-            local killed = false
-            if bossData.mount then
-                local _, _, _, _, _, _, _, _, _, _, isCollected = C_MountJournal.GetMountInfoByID(bossData.mount)
-                owned = isCollected == true and true or false
+        -- For each Rare in the Zone
+        for _, rare in ipairs(zone.rares) do
+            local killed = "|T137025:16|t "
+            if rare.quest then
+                killed = C_QuestLog.IsQuestFlaggedCompleted(rare.quest) and "|T628564:16|t " or "|T137025:16|t "
             end
-            if bossData.quest then
-                killed = C_QuestLog.IsQuestFlaggedCompleted(bossData.quest)
-            end
-            -- Boss List Item
+            local covenantRequired = rare.covenantRequired and "|T" .. zone.icon .. ":16|t |cff" .. zone.color .. C_Covenants.GetCovenantData(zone.covenant).name .. " to summon|r " or ""
+            -- Rare
             ravShadowlands:CreateLabel({
                 type = "Label",
-                name = bossData.id,
+                name = rare.id,
                 parent = Content,
-                label = (killed and "|T628564:16|t " or "|T137025:16|t ") .. bossData.name .. "|cffffffff drops " .. itemLink .. (owned and " |T628564:16|t" or "") -- .. " |cffffff00|Hworldmap:84:7222:2550|h[|A:Waypoint-MapPin-ChatIcon:13:13:0:0|a Location]|h|r",
+                label = killed .. covenantRequired .. rare.name .. " |cffbbbbbbdrops:|r"
             })
+            -- For each Item dropped by the Rare in the Zone
+            for _, item in ipairs(rare.items) do
+                local itemName, itemLink = GetItemInfo(item.id)
+                local covenantOnly = item.covenantOnly and " |cff" .. zone.color .. "only for |T" .. zone.icon .. ":16|t " .. C_Covenants.GetCovenantData(zone.covenant).name .. "|r" or ""
+                local owned = ""
+                if item.mount then
+                    local _, _, _, _, _, _, _, _, _, _, isCollected = C_MountJournal.GetMountInfoByID(item.mount)
+                    owned = isCollected == true and " |T628564:16|t" or ""
+                end
+                local guaranteed = (not owned and item.guaranteed) and " |cffbbbbbbGuaranteed!|r" or ""
+                -- Item
+                ravShadowlands:CreateLabel({
+                    type = "Label",
+                    name = rare.id .. "-items",
+                    parent = Content,
+                    label = "  " .. itemLink .. guaranteed .. covenantOnly .. owned,
+                    offsetY = -small,
+                })
+            end
         end
     end
 
